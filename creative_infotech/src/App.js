2e5fe397559fb2aa5a5f172a701e8c4cc0551f55
spark-Max <karan0513c@gmail.com>
2020-05-19 14:17:11 +0530

    Skeleton

---
+++ creative_infotech/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {HomePage} from './pages/homepage/hompage.component';
+import {Route, Switch} from 'react-router-dom';
+import './App.css';
+
+import ShopPage from './pages/shop/shop.component'
+ 
+
+function App() {
+  return (
+    <div >
+      <Switch>
+     <Route exact path = '/' component = {HomePage}/>
+     <Route exact path = '/shop' component = {ShopPage}/> 
+     </Switch>
+    </div>
+  );
+}
+
+export default App;
