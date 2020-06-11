a000f9331e9da8300c39d5c1fc0b57b61d674e81
spark-Max <karan0513c@gmail.com>
2020-06-11 08:11:46 +0530

    User Reducer setup, Cart Box and Redirection after sign in

--- creative_infotech/src/components/header-component/header.component.jsx
+++ creative_infotech/src/components/header-component/header.component.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import {connect} from 'react-redux'
 import './header.styles.scss';
 import {ReactComponent as Logo} from '../../assets/crown.svg';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import {auth} from '../../firebase/firebase.utils';
 
-const Header = ({currentUser}) => (
+const Header = ({currentUser, hidden}) => (
     <div className = 'header'>
         <Link to = '/'>
             <Logo className = 'logo'/>
@@ -22,8 +25,16 @@ const Header = ({currentUser}) => (
                 :
                 <Link className = 'option' to = '/signin'>SIGN IN</Link>
             }
+            <CartIcon />
         </div>
+        {
+            hidden ? null : <CartDropdown />
+        }
     </div>
 )
 
-export default Header;
+const mapStateToProps = ({user: {currentUser}, cart: {hidden}} ) => ({
+    currentUser,
+    hidden
+});
+export default connect(mapStateToProps)(Header);
